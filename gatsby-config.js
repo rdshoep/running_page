f89cf451c1c8ce79d77975c0b26af1701eb3f55e
rdshoep <rdshoep@gmail.com>
2023-02-03 20:53:21 +0800

    chore: 变更首页为私有化信息

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,14 @@
 module.exports = {
   pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    siteTitle: 'Sports',
+    siteUrl: 'https://sports.rdshoep.com',
+    logo: 'https://avatars.githubusercontent.com/u/7840715',
+    description: 'personal sport page',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
-      },
-      {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://blog.rdshoep.com',
       },
     ],
   },
